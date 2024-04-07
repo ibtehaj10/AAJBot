d6439ed49966def663b3143282aa66bcf99153b3
Hafiz Ibtehaj Samir Khan <51523656+ibtehaj10@users.noreply.github.com>
2024-04-07 19:42:19 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.chat_models import ChatOpenAI
 apikeys = apikey
-
+from openai import OpenAI
+from api import apikey
+client = OpenAI(api_key=apikey)
 app = Flask(__name__)
 
 
@@ -25,7 +27,7 @@ db = Chroma(persist_directory="mydb", embedding_function=embeddings)
 # embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 # db = Chroma.from_documents(docs, embedding_function)
 ############################## 
-def retrieve_combined_documents(query, max_combined_docs=4):
+def retrieve_combined_documents(query, max_combined_docs=2):
     retriever = db.as_retriever(search_type="mmr")
 
     rev_doc = retriever.get_relevant_documents(query)
@@ -44,24 +46,47 @@ def retrieve_combined_documents(query, max_combined_docs=4):
 def gpt(inp,prompt):
     
     systems = {"role":"system","content":"""
-    you are an AI Assistant you name is AAJBot, you are integrated to AA Joyland's whatsapp. your job is to answer the question from the given document about AA Joyland products. try to be specific about product
+    you are an AI Assistant you name is AAJBot, you are integrated to AA Joyland's whatsapp. your job is to answer the question from the given document about AA Joyland products.
+    try to be specific about products. you must be very friendly with user and ask how you can add fun in his family day in the start of convo.
+    
+    We Offer Birthday Celebrations at our theme parks if user ask about birthday plans then:
+                
+               - your have to ask his full name 
+               - then you need to confirm which location he's interested in to celebrate
+                            . Bounce Karachi
+                            . Giggle Town
+                            . Peekabear North Walk
+                            . Peekabear Ocean Mall
+                            . Super Space Hyderabad
+                            . Super Space Millenium Mall
+                            . Super Space Ocean Mall
+                            . Super Space Shareef Complex 
+               - after this you'll return the link of birthday packages specific to location he's interested in like 
+                        if he is intrested in Giggle Town you'll send him `www.joyland.com/giggletown`
+            - All links are here:
+                www.joyland.com/packages/bouncekhi,
+               www.joyland.com/packages/peekabearnothwalk
+               www.joyland.com/packages/peekabearoceanmall
+               www.joyland.com/packages/superspacehyderabad
+               www.joyland.com/packages/superspaceoceanmall
+               www.joyland.com/packages/superspacemillenium
+               www.joyland.com/packages/superspaceshareefcomplex
+
+
+
 """}
     rcd = retrieve_combined_documents(prompt)
     systems2 = {"role":"system","content":str(rcd)}
-    #systems3 = {"role":"system","content":"IMPORTANT : be concise as possible and do not says anything like you dont have the context or thing do not mention in document. If you cant find anything about question just redirect user to support@metutors.com email."}
-    #systems4 = {"role":"system","content":"The user is {}. answer him accordingly".format(status)}
     new_inp = inp
     new_inp.insert(0,systems)
     new_inp.insert(1,systems2)
-    #snew_inp.insert(2,systems3)
-    #new_inp.insert(3,systems4)
     print("inp : \n ",new_inp)
-    openai.api_key = apikeys
+    # openai.api_key = apikeys
     print('&&&&&&&&&&&&^^^^^^^^^^^^%$######################$%^&*((((((((((((((()))))))))))))))')
-    completion = openai.ChatCompletion.create(
-    model="gpt-4-1106-preview", 
-    messages=new_inp
-    )
+    completion = client.chat.completions.create(
+    model="gpt-4-turbo-preview", 
+    messages=new_inp)
+    print(completion)
     return completion
 
 ############    GET CHATS BY USER ID ##################
@@ -94,7 +119,7 @@ def write_chat(new_data, id):
 
 
 ################################ CHECK IF USER IS ALREADY EXIST IF NOT CREATE ONE ELSE RETURN GPT REPLY ##################
-@app.route('/api', methods=['POST'])
+@app.route('/chat', methods=['POST'])
 def check_user():
     
     ids = request.json['user_id']
@@ -112,9 +137,9 @@ def check_user():
         chats = get_chats(path)
         print(chats)
         send = gpt(chats,prompt)
-        reply = send.choices[0].message
-        print("reply    ",reply.content)
-        write_chat({"role":"assistant","content":reply.content},path)
+        reply = send.choices[0].message.content
+        print("reply    ",reply)
+        write_chat({"role":"assistant","content":reply},path)
         return {"message":reply,"status":"OK"}
         # except:
         #     return {"message":"something went wrong!","status":"404"}
@@ -148,5 +173,5 @@ def get_chatss():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5002)
+    app.run(port=5008)
     
--- wapp.py
+++ wapp.py
@@ -6,6 +6,7 @@ import requests
 # import mysql.connector
 
 
+
 def gpt(uid,text):
     headers={'Content-Type': 'application/json'}
     body = {
@@ -13,12 +14,12 @@ def gpt(uid,text):
         "prompt":text
 
     }
-    r = requests.post('http://127.0.0.1:5002/chat',headers=headers,json=body)
+    r = requests.post('http://127.0.0.1:5008/chat',headers=headers,json=body)
     print(r)
     ans = r.json()
     print(ans)
     if 'filename' not in ans:
-        anss = ans['message']['content']
+        anss = ans['message']
         return anss
     else:
         return ans
@@ -28,8 +29,8 @@ def gpt(uid,text):
 
 flask_app = Flask(__name__)
 wa = WhatsApp(
-    phone_id='229272100273078',
-    token='EABkWqVSvZBZCcBO6j0ZBwTTZClCftAd9FgCeazZCSQnXGilgGj6BGTm2MDynamTv7z4EzdnkCZB23w4GNZCSDZC8TVNImzKCAcvuoe1Ad3i3oAEqZCneAPY7UmjKZCN54UFntDLrfbXlycd9O7EkzGKOntix6EpBhGRBtT6xKuZBZBe3hsZCHmpLK6D3KcZAYEQHpfdUCapLDBjleSSD3QTQ94EzgZD',
+    phone_id='109683645280921',
+    token='EAAPjgUQ6VZCIBOzYYvL1TVHsnBXWk1ZC9sJ9vcjwmVZCMKx3crF3rm2DHlklp4cWv6aIUp3p2MpRgwFdqzE5irdQvRd9oKBSzA9c8pUANtUPcwC8LQRoZB02728cTB1VJiyuZAQAht8NXUnLAZAhOCZCHrr1cfT7ZB0jmKWZBxdUmRsQNH5AE22nZCNRiu4XT1HZAzSUYVZCqHCgxRXkbBpdZAb0ZD',
     server=flask_app,
     verify_token='asd',
 )
@@ -41,21 +42,47 @@ def hello(client: WhatsApp, message: Message):
     uid = message.from_user.wa_id
     msg = message.text
     reply = gpt(uid,msg)
-    if type(reply) == str:
+    base_url ='www.joyland.com/packages'
+    # if type(reply) == str:
+    if base_url not in reply:
         message.reply_text(
             text=reply,
         )
     else:
+        filename= ""
+        if 'bounce' in reply :
+            filename = 'Bouncekhi.pdf'
+        elif 'giggle' in reply:
+            filename ="Giggle Town.pdf"
+        elif 'northwall' in reply:
+            filename = "Peekabear North Walk.pdf"
+        elif 'peekabearocean' in reply:
+            filename = "Peekabear Oocean Mall.pdf"
+        elif 'hyderabad' in reply.lower():
+            filename = "Super Space Hyderabad.pdf"
+        elif 'millennium' in reply.lower():
+            filename = "Super Space Millennium Mall.pdf"
+        elif 'superspaceocean' in reply.lower():
+            filename = "Super Space Ocean Mall.pdf"
+        elif 'shareef' in reply.lower():
+            filename = "Super Space Shareef Complex.pdf"
+
+
+
+
+
         message.reply_document(
-        "invoice.pdf",
+        f"birthday/{filename}",
         # document="invoice.pdf",
-        body="invoice.pdf"
-       
-    )
-    print('pdf send')
+        body=filename
+        )
+
+
+
+    print('pdf send',filename)
 
 # @wa.on_callback_button(CallbackFilter.data_startswith('id'))
 # def click_me(client: WhatsApp, clb: CallbackButton):
 #     clb.reply_text('You clicked me!')
 
-flask_app.run()  # Run the flask app to start the webhook
+flask_app.run(port=5003)  # Run the flask app to start the webhook
